70b0ddcae3dba5214874bde76f2a46d069b00bbd
willianbogarin <willianbogarin@hotmail.com>
2023-08-05 10:28:17 +0200

    Initial Commit

---
+++ 2nd TestGit Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  2nd TestGit Project
+//
+//  Created by Willian Bogarin Jr on 2023. 08. 05..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2nd TestGit Project/_nd_TestGit_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _nd_TestGit_ProjectApp.swift
+//  2nd TestGit Project
+//
+//  Created by Willian Bogarin Jr on 2023. 08. 05..
+//
+
+import SwiftUI
+
+@main
+struct _nd_TestGit_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
